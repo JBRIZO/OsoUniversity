42045ce0f7b238bbc6204f289310b9acc350f3bc
JBRIZO <jonathan.laux@hotmail.com>
2020-04-27 00:54:13 -0600

    commit

--- src/edu/ujcv/progra1/Menu.java
+++ src/edu/ujcv/progra1/Menu.java
@@ -18,8 +18,29 @@ public class Menu
     public void evaluacionTernas()
     {
         System.out.println("           Evaluar ternas");
+        System.out.println("2. Clase G");
 
     }
-
+    public void alumnos()
+    {
+        System.out.println("               Alumnos");
+        System.out.println("1. Alumnos matriculados");
+        System.out.println("2. Alumnos expulsados");
+        System.out.println("3. Alumnos egresados");
+        System.out.println("4. Salir");
+    }
+    public  void consultarClase()
+    {
+        System.out.println("               Clases");
+        System.out.println("1. CLASE A ");
+        System.out.println("2. CLASE B");
+        System.out.println("3. CLASE C");
+        System.out.println("4. CLASE D");
+        System.out.println("5. CLASE E");
+        System.out.println("6. CLASE F");
+        System.out.println("7. CLASE G");
+        System.out.println("8. MENU PRINCIPAL");
+        System.out.println(" ");
+    }
 
 }
--- src/edu/ujcv/progra1/alumnos.java
+++ src/edu/ujcv/progra1/alumnos.java
@@ -6,12 +6,85 @@ package edu.ujcv.progra1;
 //encapsula un objeto
 public class alumnos {
     private static int corelativo = 0;
-    private int id;
+    private String id;
     private String nombre;
     private String apellido;
+    private boolean evaluacionA;
+    private boolean evaluacionB;
+    private boolean evaluacionC;
+    private boolean evaluacionD;
+    private boolean evaluacionE;
+    private boolean evaluacionF;
+    private boolean evaluacionG;
+    private int contReprobadas;
+    private int contParciales;
+
+    public int getContParciales(){
+        return contParciales;
+    }
+    public void setContParciales(int contParciales) {
+        this.contParciales = contParciales;
+    }
+
+    public int getContReprobadas() {
+        return contReprobadas;
+    }
+
+    public void setContReprobadas(int contReprobadas) {
+        this.contReprobadas = contReprobadas;
+    }
+
+    public boolean getEvaluacionA(){
+        return evaluacionA;
+    }
+    public void setEvaluacionA(boolean evaluacionA) {
+        this.evaluacionA = evaluacionA;
+    }
+
+    public boolean getEvaluacionB(){
+        return evaluacionB;
+    }
+    public void setEvaluacionB(boolean evaluacionB) {
+        this.evaluacionB = evaluacionB;
+    }
+
+    public boolean getEvaluacionC(){
+        return evaluacionC;
+    }
+    public void setEvaluacionC(boolean evaluacionC) {
+        this.evaluacionC = evaluacionC;
+    }
+
+    public boolean getEvaluacionD(){
+        return evaluacionD;
+    }
+    public void setEvaluacionD(boolean evaluacionD) {
+        this.evaluacionD = evaluacionD;
+    }
+
+    public boolean getEvaluacionE(){
+        return evaluacionE;
+    }
+    public void setEvaluacionE(boolean evaluacionE) {
+        this.evaluacionE = evaluacionE;
+    }
+
+    public boolean getEvaluacionF(){
+        return evaluacionF;
+    }
+    public void setEvaluacionF(boolean evaluacionF) {
+        this.evaluacionF = evaluacionF;
+    }
+
+    public boolean getEvaluacionG(){
+        return evaluacionG;
+    }
+    public void setEvaluacionG(boolean evaluacionG) {
+        this.evaluacionG = evaluacionG;
+    }
 
 
-    public int getId(){
+    public String getId(){
         return id;
     }
 
@@ -19,13 +92,23 @@ public class alumnos {
 
     public String getApellido(){return apellido;}
 
+// metodos
+
+    public void parcial(){
+        this.contParciales++;
+    }
+
+    public void reprobar(){
+        this.contReprobadas++;
+    }
+
 
     public void appendApellido(String apellido){
         this.apellido += "\n"+ apellido;
     }
 
-    alumnos(String nombre, String apellido){
-        id = corelativo++;
+    alumnos(String nombre, String apellido, String id){
+        this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
     }
@@ -37,6 +120,10 @@ public class alumnos {
     public String mostrarResumido(){
         return nombre;
 
+    }
+    @Override
+    public String toString(){
+        return getNombre() + " " + getApellido() + " " + getId();
     }
 
     public String mostrarVerbose() {
